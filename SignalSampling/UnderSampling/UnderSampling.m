cc35df0aae789ad0a7a105a01ddf665136dbfa4f
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-05 15:30:04 +0530

    Added Under Sampling

---
+++ SignalSampling/UnderSampling/UnderSampling.m
@@ -0,0 +1,24 @@
+clc;
+clear all;
+clf;
+
+t=0:0.001:2;
+x=sin(2*pi*2*t);
+fmax=4;
+
+fs1=3
+n1=0:(1/fs1):2;
+
+x1=sin(2*pi*2*n1);
+
+subplot(2,1,1);
+plot(t,x,'g');
+xlabel('Time');
+ylabel('Amplitude');
+title('Original signal');
+
+subplot(2,1,2);
+stem(n1,x1,'b');
+xlabel('n');
+ylabel('Amplitude');
+title('Under Sampling (fs=2)');
