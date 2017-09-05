635288747dc1873ea35f4b23261b96cf794a7bfe
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-09-05 16:56:24 +0530

    Added Error in Signals

---
+++ Error/Error.m
@@ -0,0 +1,27 @@
+clc;
+clear all;
+clf;
+
+t=0:0.01:2;
+x=sin(2*pi*2*t);
+y=sin(pi*2*t);
+
+error=x-y;
+
+subplot(3,1,1)
+stem(t,x,'g');
+xlabel('Time');
+ylabel('Amplitude');
+title('X');
+
+subplot(3,1,2);
+stem(t,y,'b');
+xlabel('Time');
+ylabel('Amplitude');
+title('Quantaized X(3 bits)');
+
+subplot(3,1,3);
+stem(t,abs(error),'r');
+xlabel('Time');
+ylabel('Amplitude');
+title('Error in Actual and Quantized graphs');
