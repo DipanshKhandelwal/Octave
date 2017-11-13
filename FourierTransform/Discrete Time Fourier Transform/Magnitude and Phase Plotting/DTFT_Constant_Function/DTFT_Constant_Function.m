9962e50142d1caf44c049fd151d5bf4f9dfb48ff
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-14 01:11:33 +0530

    Changed Constant Function

--- FourierTransform/Discrete Time Fourier Transform/Magnitude and Phase Plotting/DTFT_Constant_Function/DTFT_Constant_Function.m
+++ FourierTransform/Discrete Time Fourier Transform/Magnitude and Phase Plotting/DTFT_Constant_Function/DTFT_Constant_Function.m
@@ -6,19 +6,19 @@ n = -10 : 1 : 10;
 l = length(n);
 y = ones(1,l);
 
-r = -5*pi : 2*pi : 5*pi;
-x = 2*pi*ones(1, length(r));
+r = -10 : 1 : 10;
+x = fftshift(fft(y))
 
 #Plotting input
 subplot(2, 1, 1);
-stem(n, y, 'b');
+plot(n, y, 'b');
 xlabel("n");
 ylabel("Amplitude");
 title("Constant Function");
 
 #Plotting output
 subplot(2, 1, 2);
-stem(r, x, 'b');
+stem(r, abs(x), 'b');
 xlabel("n");
 ylabel("Amplitude");
 title("Signal in frequency domain");
