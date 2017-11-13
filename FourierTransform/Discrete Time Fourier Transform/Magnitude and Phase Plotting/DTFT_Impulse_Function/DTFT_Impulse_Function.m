6587a90a91dc6c5c3c2822093997e7c86be7a268
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-14 01:11:53 +0530

    Changed Impulse Function

--- FourierTransform/Discrete Time Fourier Transform/Magnitude and Phase Plotting/DTFT_Impulse_Function/DTFT_Impulse_Function.m
+++ FourierTransform/Discrete Time Fourier Transform/Magnitude and Phase Plotting/DTFT_Impulse_Function/DTFT_Impulse_Function.m
@@ -2,23 +2,22 @@ clc;
 clear all;
 clf;
 
-w = -4*pi : 4*pi;
-l = length(w);
-y = ones(1,l);
+w = -10 : 1 : 10;
+y = [zeros(1,10) 1 zeros(1, 10)]
 
-n = -(l/2) : 1 : (l/2) - 1;
-x = [zeros(1, (l/2)) 1 zeros(1, (l/2)-1)];
+n = -10 : 1 : 10;
+x = fftshift(fft(y))
 
 #Plotting input
 subplot(2, 1, 1);
-stem(n, x, 'b');
+stem(w, y, 'b');
 xlabel("n");
 ylabel("Amplitude");
 title("Impulse Signal");
 
 #Plotting output
 subplot(2, 1, 2);
-stem(w, y, 'b');
+plot(n, abs(x), 'b');
 xlabel("n");
 ylabel("Amplitude");
 title("Impulse Signal");
