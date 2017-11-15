f758cd36f7ac71c33bcb2321b248d030957cdc4d
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-15 19:15:07 +0530

    Added low pass sound filter

---
+++ Sound/SoundFilters/LowPassFilter/LowPassSoundFilter.m
@@ -0,0 +1,30 @@
+clc ;
+clear all;
+clf;
+close all;
+
+[y,fs]=wavread('audio.wav');
+t=linspace(0,length(y)/fs,length(y));
+plot(t,y);
+title('Original sound')
+xlabel('time')
+ylabel('samples')
+
+Nfft=1024;
+f=linspace(0,fs,Nfft);
+m=abs(fft(y,Nfft));
+figure
+plot(f(1:Nfft/2),m(1:Nfft/2))
+title('Fourier Transform')
+
+N_order=10;
+R=120;
+fc=2000;
+WST=2*fc/fs;
+
+#lowpass filtering
+[DEN_bas,NUM_bas]=cheby2(N_order,R,WST,'low');
+M_bas=filter(DEN_bas,NUM_bas,y);
+
+#writing the file
+wavwrite(M_bas,fs,16,'/home/Desktop/newAudio')
